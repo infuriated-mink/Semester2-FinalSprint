cf0cfdc04ac6caa1c7ccde58d77a2e095883a91f
Vanessa Rice <vanessa.rice@keyin.com>
2023-12-11 15:11:19 -0330

    fixed login page

--- workout-wizard/src/components/login.jsx
+++ workout-wizard/src/components/login.jsx
@@ -1,85 +1,51 @@
-import React, { useEffect, useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
-import { ToastContainer, toast } from 'react-toastify';
-import "../css/login.css";
-import 'react-toastify/dist/ReactToastify.css';
-import userData from "../data/user.json";
+import React from "react";
+import { Link } from "react-router-dom";
 
-export default function Auth() {
-  const navigate = useNavigate();
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [loginError, setLoginError] = useState("");
-  const [isToastOpen, setIsToastOpen] = useState(true);
-
-  useEffect(() => {
-    // Check if user is already logged in (via local storage)
-    const storedUser = localStorage.getItem("loggedInUser");
-    if (storedUser) {
-      navigate("/home");
-    }
-  }, [navigate]);
-
-  const notify = (user) => {
-    setIsToastOpen(true);
-    toast(
-      `Welcome ${user.firstName}`,
-      { position: toast.POSITION.TOP_CENTER },
-      { autoclose: 4000 },
-      { onClose: () => setIsToastOpen(false) }
-    )
-  };
-
-  function wait(time) {
-    return new Promise(resolve => {
-      setTimeout(resolve, time);
-    });
-  }
-
-  const storeLoggedInUser = (user) => {
-    localStorage.setItem("loggedInUser", JSON.stringify(user));
-  };
-
-  async function goToPage() {
-    await wait(5000);
-    navigate(`/home`);
-  }
-
-  const handleLogin = () => {
-    if (userData[username] && userData[username].password === password) {
-      const user = userData[username];
-      notify(user);
-      storeLoggedInUser(user);
-      goToPage();
-    } else {
-      setLoginError("Username and password do not match.");
+export default function Login({ changeAuthMode }) {
+  const handleClick = () => {
+    // Ensure that the changeAuthMode function is called
+    if (changeAuthMode) {
+      changeAuthMode(); // This should switch the mode to "signup"
     }
   };
 
   return (
-    <div className="mainbox">
-      {/* ... rest of your code ... */}
-      <div className="Auth-form-container">
-        <form className="Auth-form">
-          {/* ... rest of your form ... */}
+    <div className="Auth-form-container">
+      <form className="Auth-form">
+        <div className="Auth-form-content">
+          <h3 className="Auth-form-title">Sign In</h3>
+          <div className="text-center">
+            Not registered yet?{" "}
+            <Link to="/signup" className="link-primary" onClick={handleClick}>
+              Sign Up
+            </Link>
+          </div>
+          <div className="form-group mt-3">
+            <label>Email address</label>
+            <input
+              type="email"
+              className="form-control mt-1"
+              placeholder="Enter email"
+            />
+          </div>
+          <div className="form-group mt-3">
+            <label>Password</label>
+            <input
+              type="password"
+              className="form-control mt-1"
+              placeholder="Enter password"
+            />
+          </div>
           <div className="d-grid gap-2 mt-3">
-            <button
-              type="button"
-              className="btn btn-primary"
-              onClick={handleLogin}
-            >
+            <button type="submit" className="btn btn-primary">
               Submit
             </button>
           </div>
-          <p className="forgot-password text-right mt-2">
-            {loginError && <span className="error-message">{loginError}</span>}
-            <br />
+          <p className="text-center mt-2">
+            Forgot <a href="#">password?</a>
           </p>
-          <div className="text-center mt-3">
-          </div>
-        </form>
-        <ToastContainer />
-      </div>
+        </div>
+      </form>
     </div>
   );
 }
