c540b9c618da46a8465ae1702d0c111571ffdcb0
JeeecobTheAlien <125674135+JeeecobTheAlien@users.noreply.github.com>
2023-12-20 17:34:37 -0330

    Push it Harder

--- workout-wizard/src/components/homedropdown.jsx
+++ workout-wizard/src/components/homedropdown.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import '../css'
+import '../css/homedropdown.css'
 
 const HomeDropdown = () => {
   const navigate = useNavigate();
